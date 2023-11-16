cee833d5cc8ab5d3a31b69b927d0492aad1c68bd
e6m9 <alostsound@gmail.com>
2023-11-16 14:26:38 -0500

    save button and date

--- assets/script.js
+++ assets/script.js
@@ -19,5 +19,24 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+  // DONE
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var fieldInput = document.querySelector('#description');
+var saveBtn = document.querySelector('.saveBtn');
+
+saveBtn.addEventListener("click", saveText);
+
+function saveText() {
+var description = fieldInput.value;
+
+localStorage.setItem("description", description);
+}
+
+
+// Day.js to 
+
+var today = dayjs();
+var element = document.getElementById('currentDay');
+element.textContent = today.format('MMM D, YYYY');
