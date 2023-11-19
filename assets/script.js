1a03822dab23d1d2aae16a21752ae3de3d19874e
e6m9 <alostsound@gmail.com>
2023-11-19 18:59:34 -0500

    comments added

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,36 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// code set inside a function to tell it wait until everything is loaded into the DOM before doing anything else
 document.addEventListener('DOMContentLoaded', function () {
-  // DONE
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //DONE
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // DONE
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // DONE
-  // TODO: Add code to display the current date in the header of the page.
 
-
+  // grabs the hourContainer div, sets up an array, and sets the parameters for creating the hour blocks 
   var blockBody = document.getElementById('hourContainer');
   var blocks = [];
   var firstHour = 9;
   var hours = 9;
 
-  // for loop to generate the hour blocks and the elements inside them, giving them the class name "hour- '(value of) currentHour'", and pushing them to the block array
+  // for loop to generate the hour blocks and the elements inside them, giving them the class name "hour-'(value of) currentHour'", and pushing them to the blocks array
   for (var i = 0; i < hours; i++) {
     var currentHour = firstHour + i;
+
+    // if the current hour is less than 12, amPM is AM, otherwise, it's PM
     var amPM = currentHour < 12 ? 'AM' : 'PM';
 
+    // creates the hourblock div and gives it an id based on the index and adds the Bootstrap classes
     var hourBlock = document.createElement('div');
     hourBlock.id = 'hour-' + currentHour;
     hourBlock.classList.add('row', 'time-block');
 
+    // creates the blockTitle element which displays the time with an argument that converts hours greater than 12 into their 12 hour equivalent appended with amPM
     var blockTitle = document.createElement('div');
     blockTitle.classList.add('col-2', 'col-md-1', 'hour', 'text-center', 'py-3');
     blockTitle.textContent = (currentHour % 12 || 12) + amPM;
 
+    // adds a unique textarea id to each hour block
     var textarea = document.createElement('textarea');
     textarea.classList.add('col-8', 'col-md-10');
     textarea.id = 'description-hour-' + currentHour;
     textarea.rows = 3;
 
+    // creates save button and save button icon
     var saveBtn = document.createElement('button');
     saveBtn.classList.add('btn', 'saveBtn', 'col-2', 'col-md-1');
     saveBtn.setAttribute('aria-label', 'save');
@@ -58,31 +39,42 @@ document.addEventListener('DOMContentLoaded', function () {
     saveIcon.classList.add('fas', 'fa-save');
     saveIcon.setAttribute('aria-hidden', 'true');
 
+    // adds all the created elements to the hourBlock div and the save icon to the save button
     saveBtn.appendChild(saveIcon);
     hourBlock.appendChild(blockTitle);
     hourBlock.appendChild(textarea);
     hourBlock.appendChild(saveBtn);
 
+    // adds the full hourBlock div to the blockBody div
     blockBody.appendChild(hourBlock);
 
+    // adds a call to the saveText function to the save button
     saveBtn.addEventListener("click", saveText);
 
+    // pushes the completed hourBlock elements into the blocks array
     blocks.push(hourBlock);
 
+    // grabs the id from each element in the blocks array to appy the displayText function to each id accordingly
     blocks.forEach(function (block) {
       var blockID = block.id;
       displayText(blockID);
     })
+    
+    // runs the checkTime function after generating the elements so that they can get their past, present, or future class upon page load
     checkTime();
   }
 
+  // checks the realtime against each blockId
   function checkTime() {
     var currentHour = dayjs().hour();
+    // code to test functionality of the function outside of planner hours
     // var currentHour = dayjs().set('hour', 12).hour();
 
+    // parses out the number of the blockIds
     blocks.forEach(function (hourBlock) {
       var blockHour = parseInt(hourBlock.id.split('-')[1]);
 
+      // clears any past, present, or future class before determining where the blockHour currently is and what class it should receive
       hourBlock.classList.remove('past', 'present', 'future');
 
       if (blockHour < currentHour) {
@@ -95,10 +87,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // the checkTime function is called every minute
   setInterval(function () {
     checkTime.call(this);
   }, 60000);
 
+  // grabs the value of the textarea based on which save button is clicked, labels and saves that value with an appropriate id and adds a message to tell the user it has saved to local storage, then it clears the text area and runs displayText
   function saveText() {
     var description = this.parentNode.querySelector('textarea').value;
     var blockID = this.parentNode.id;
@@ -110,6 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
     displayText(blockID);
   }
 
+  // tied to the specific blockId based on the save button pressed and adds the appropriate text content from local storage to the appropriate text field
   function displayText(blockID) {
     var fieldInput = document.getElementById('description-' + blockID);
 
@@ -119,8 +114,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  // Day.js to get the current time and set as a variable to check against
-
+  // finds and displays the current date in the header id 'currentDay'
   var today = dayjs();
   var element = document.getElementById('currentDay');
   element.textContent = today.format('dddd, MMMM DD');
